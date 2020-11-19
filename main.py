c717fd39ccc4203732aa20c7dda59325d97484da
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-11-19 15:16:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ async def on_ready():
 	    
 @client.command()
 async def q(ctx,que: str):
-	     embed=discord.Embed(title="", description=f"Question {que} Is Comming On 📱", color=discord.Color.red())
-	     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/702945721610928219/704569122956247131/700566617456902146.gif")
+	     embed=discord.Embed(title="**__QUESTION ALERT__**", url="https://discord.gg/2degbQMAxU", description=f"**Question No. {que} is comming soon on your mobile screen.**", color=discord.Color.red())
+	     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/775385239245160478/778865719437164544/help-147419__340.png")
 	     embed.set_image(url="https://cdn.discordapp.com/attachments/691596509539598379/692639381109997618/HmjViJc.gif")
-	     embed.set_footer(text="©Server Manager | Myran#0001")
-	     await ctx.send(content="@everyone",embed=embed)
+	     embed.set_footer(text="Developed by Subrata#3297", icon_url="https://cdn.discordapp.com/attachments/775385239245160478/778865719437164544/help-147419__340.png")
+	     await ctx.send(embed=embed)
 
 @client.command()
 async def hq(ctx,time=None, accuracy=None, ques=None, prize=None):
@@ -46,7 +46,7 @@ async def hq(ctx,time=None, accuracy=None, ques=None, prize=None):
 		embed.set_thumbnail(url="") #Put Your Thumbnail url
 		await ctx.send(embed=embed)
 
-client.run('NzczOTI4NTc1NzAyNzk0MjUx.X6QXJw.r-U_XbSALwkVzSxK7zg0DTNytvU')
+#client.run('NzczOTI4NTc1NzAyNzk0MjUx.X6QXJw.r-U_XbSALwkVzSxK7zg0DTNytvU')
 
 #BOT_OWNER_ROLE = 'Runner' # change to what you need
 #BOT_OWNER_ROLE_ID = "503197827556704268" 
